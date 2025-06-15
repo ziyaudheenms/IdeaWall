42e48d6dedca8e84411b91c309505e32ee715d35
Ziyaudheen <shajeem229@gmail.com>
2025-06-15 20:16:38 +0530

    second commit

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/elements/NavBar";
+import Navbar from "../elements/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
